c6d6519eae4b78b295f4318eaf9806126a1d2e7b
Wintus <132e84e5@opayq.com>
2015-01-27 22:12:55 -0800

    Enhance homework 2
    out of class

--- homework2.py
+++ homework2.py
@@ -4,65 +4,141 @@ def order_calculator():
     """The main function.
 
     This function gets 3 inputs of items.
-    Each lines of inputs consists of
-    description, price, quantity, weight per item.
-    Then it display the items, SubTotal, Shipping costs, the Tax, and the Total."""
+    Each line of inputs consists of
+        description, price, quantity, weight per item.
+    Then, it display the items, SubTotal, Tax, Shipping costs, and the Total.
+    Success: () -> __Finished__ | () -> __ERROR__"""
 
-    #literals
+    #Literals
     ship_rate = .25 #($/lb)
     handling_charge = 5.0 #per order
-    tax = 8.5/100 #% in rate
+    tax = 8.5 / 100 #% to rate
+
+    #Each item is represented by a dictoinary type
+##    dic_item = {"id":0, "name":"dummy",\
+##                "price":0.0, "quantity":0, "weight":0.0}
+    #list_items is a list of dictionaries like dic_item
+    print("Enter your order of each items.")
+    print("Inputs should follow the format: " + \
+          "Description Price Quantity Weight(in pounds)")
+    print("Enter nothing to proceed to cashing.")
+    #Make a order list of items
+    list_items = []
+    list_input = []
+    count = 0
+    while True:
+        item = dict()
+            #Initialize by creating a new dictionary because of the reference
+        list_input = input("Enter the item No.{}: ".format(count+1)).split()
+        if list_input == []:
+            print("> Proceed to cashing\n")
+            break
+        #Input values into the 'n'th dictionary in the item list
+        try: #Error check
+##            item["id"]      = int(count)
+            item["name"]    = str(list_input[0])
+            item["price"]   = float(list_input[1])
+            item["quantity"]= int(list_input[2])
+            item["weight"]  = float(list_input[3])
+            list_items.append(item)
+            #Show the last inputed item
+            print("> {} ${:,.2f} {:,} each {:,.1f}lbs\n"\
+                  .format(item["name"], item["price"],\
+                          item["quantity"], item["weight"]
+                          )
+                  )
+            count += 1
+        except IndexError:
+            print("Lack of value")
+            print("Please Input all required information.\n")
+        except ValueError:
+            print("Invalid value")
+            print("Input information correctly.\n")
+        except:
+            print("UNKNOWN ERROR")
+            return "__ERROR__"
+
+    #Skip when the order is empty
+    if list_items == []:
+        print("You purchased nothing. End the order.")
+        return "__Finished__"
+
+    #Calculate the costs
+##    total = 0.0
+##    subtotal = 0.0
+##    total_weight = 0.0
+
+    #Calc the subtotal, the total weight, the tax,
+        #and the shipping and handling cost
+    
+    def zipWith_each_items(func, list_of_items, key1, key2):
+        """Multiple 2 value of the 2 given keys"""
+        return sum(map(lambda dic1, dic2 : func(dic1[key1], dic2[key2]),\
+##                       list_of_items, list_of_items))
+                       *([list_of_items] * 2))) #Omit the duplicate
+
+    def multi(a, b):
+        """Multiply"""
+        return a * b
+    
+    subtotal = zipWith_each_items(multi, list_items, "price", "quantity")
+    total_weight = zipWith_each_items(multi, list_items, "weight", "quantity")
+
+    #Tax
+    items_tax = subtotal * tax
+    #Shipping and Handling costs
+    ship_handl = ship_rate * total_weight + handling_charge
+
+    #Sums up subtotal, tax, shipping fee and handling charge
+    total = subtotal + items_tax + ship_handl
+    
+    #Display the order
+    
+    #Define modifier functions
+    def insert_last_and(list_str): # [str] -> [str]
+        """Combine last 2 strings with ' and '
+
+    Ex. ['a', ... , 'y', 'z'] -> ['a', ... , 'y and z']"""
+        if len(list_str) == 1: return list_str # for more than 1
+        last_two = " and ".join(list_str[-2:])
+            #Slice out the lest 2 and join them with ' and '
+        return list_str[:-2] + [last_two]
+
+    def plural_form(n, str_thing):
+        """Support modifying singleton, plural form
+
+    Ex. 1 -> (1, thing) | int n -> (n, thing's') | ERROR"""
+        if n == 1:
+            return (n, str_thing)
+        elif type(n) == type(1):
+            return (n, str_thing + 's')
+        else:
+            return ValueError
+
+    #Print the order
+    print("You have purchased {} {}: ".format(*plural_form(count, 'item')) + \
+          ', '.join( # [<str>] -> <str>
+              insert_last_and([item["name"] for item in list_items])
+                  #Use list comprehention of making a list of item descriptions
+                    ) + '.'
+          )
+    print("The subtotal is ${:,.2f}.".format(subtotal))
+    print("The Shipping and Handling costs are ${:,.2f}.".format(ship_handl))
+    print("The Tax is ${:,.2f}.".format(items_tax))
+    print("The Total is ${:,.2f}.".format(total))
+    return '__Finished__'
+
+if __name__ == '__main__': #Run only in a direct call
+    order_calculator()
 
     #Sample Imput:
     #Shovel $25.00 1 each 6lbs
     #Planter $45.00 2 each 11lbs
     #Broom $12.00 1 each 4lbs
-    item_numbers = 3 #static
-    list_items = list(range(item_numbers)) #hash is preferable
-    print("Enter the order of {} items.")
-    print("Inputs should follow the format: Description Price Quantity Weight(in pounds)")
-    #Make a order list of items
-    for i in range(item_numbers):
-        list_items[i] = input("Enter the item No.{}: ".format(i+1)).split()
-        print("> {} ${:.2f} {} each {}lbs\n"\
-              .format(str(list_items[i][0]), float(list_items[i][1]),\
-                      int(list_items[i][2]), int(list_items[i][3])))
-
-    #Calculate the costs
-    total = 0.0
-    subtotal = 0.0
-    total_weight = 0.0
-    #total_items = item_numbers # == 3
     
-    for i in range(item_numbers):
-        subtotal += float(list_items[i][1]) * int(list_items[i][2])
-
-    for i in range(item_numbers):
-        total_weight += int(list_items[i][2]) * float(list_items[i][3])
-
-    #sums up subtotal, shipping fee, handling charge, and tax
-    #subtotal
-    total += subtotal
-    #Tax
-    items_tax = subtotal * tax
-    total += items_tax
-    #Shipping and Handling costs
-    ship_handl = ship_rate * total_weight + handling_charge
-    total += ship_handl
-    
-    #Display the order
     #Sample Output:
     #You have purchased: Shovel, Planter and Broom.
     #The subtotal is $127.00
     #The Shipping and Handling costs are $13.00
-    #The Tax is $11.90 
-    #The Total is $151.90.
-    print("You have purchased: {}, {}, and {}."\
-          .format(list_items[0][0], list_items[1][0], list_items[2][0]))
-    print("The subtotal is ${:.2f}.".format(subtotal))
-    print("The Shipping and Handling costs are ${:.2f}."\
-          .format(ship_handl))
-    print("The Tax is ${:.2f}.".format(items_tax))
-    print("The Total is ${:.2f}.".format(total))
-
-order_calculator()
+    #The Tax is $10.795 ($11.90)
+    #The Total is $150.795 ($151.90).
