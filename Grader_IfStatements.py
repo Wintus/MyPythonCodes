399f885bbc71fa33a70b30e455de8544560383dd
Wintus <132e84e5@opayq.com>
2015-01-27 21:27:32 -0800

    Modify grader
    in class

--- Grader_IfStatements.py
+++ Grader_IfStatements.py
@@ -1,37 +1,39 @@
 """Dicision Sturcture (If Statement)"""
 
-def grade(*args):
-    """Get a score (100-point) form the imput, and return a letter grade
+def grade(score):
+    """Get a score (100-point), and return a letter grade.
 
-    90-100:A, 80-89:B, 70-79:C, 60-69:D"""
+    90-100:A, 80-89:B, 70-79:C, 60-69:D
+    Return String"""
 
+    def is_in_range(n, tpl_range):
+        """Get a number and a tuple and check to be in the range.
+
+    The range is lower-closed upper-open range: [a, b).
+    Return Boolian"""
+        return tpl_range[0] <= n < tpl_range[1]
+    
+    #Grade the score
+    if 100 < score:
+        print("Invalid score.")
+        return '__ERROR__'
+    elif is_in_range(score, (90, 100)) or score == 100: #in [90, 100]
+        return 'A'
+    elif is_in_range(score, (80, 90)): #in [80,90)
+        return 'B'
+    elif is_in_range(score, (70, 80)): #in [70,80)
+        return 'C'
+    elif is_in_range(score, (60, 70)): #in [60,70)
+        return 'D'
+    elif is_in_range(score, (0, 60)): #in [0, 60)
+        return "No Grade"
+    else:
+        print("Invalid score.")
+        return '__ERROR__'
+    
+
+if __name__ == '__main__':
     #Get a score
     fltScore = float(input("Enter your score: "))
 
-    strRepeat = "Your grade is"
-
-    #Grade the score
-    if 100 < fltScore:
-        print("Invalid score.")
-        return -1
-    elif 90 <= fltScore: #in [90, 100]
-        print(strRepeat, 'A.')
-        return 0
-    elif 80 <= fltScore: #in [80,90)
-        print(strRepeat, 'B.')
-        return 1
-    elif 70 <= fltScore: #in [70,80)
-        print(strRepeat, 'C.')
-        return 2
-    elif 60 <= fltScore: #in [60,70)
-        print(strRepeat, 'D.')
-        return 3
-    elif 0 <= fltScore: #in [0, 60]
-        print("No grade.")
-        return 9
-    else:
-        print("Invalid score.")
-        return -1
-
-if __name__ == '__main__':
-    grade()
+    print("Your grade is {}.".format(grade(fltScore)))
