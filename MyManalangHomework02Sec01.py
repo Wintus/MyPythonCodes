3331df3f9ceab5320f4bbbb5ecc27c714d054202
Wintus <132e84e5@opayq.com>
2015-01-27 22:05:51 -0800

    Refactor
    in class

--- MyManalangHomework02Sec01.py
+++ MyManalangHomework02Sec01.py
@@ -1,30 +1,40 @@
+# Project:      Homework 02 (MaemichiManalangHomework02Sec01.py)
+# Name:         Maemichi, Yuya & Martina Manalang
+# Date:         04/28/14
+# Description:  This program will allow the user enter information about 3 items and receive their subtotal, tax, shipping and handling and total.
+
 def main():
     strDescription = ""
     intSubtotal = 0
-    
+    intTotalWeight = 0
+    intShipping = 0
+    intTax = 0
     
     for i in range(3):
-       strPurchase = str(input("Please enter the item description:"))
-       intPrice = int(input("PLease enter the price: $"))
-       intQuantity = int(input("Please enter the quantity:"))
-       intWeight = int(input("Please enter the weight per item(lbs):"))
+       strPurchase = str(input("Please enter the item description: "))
+       intPrice = int(input("PLease enter the price:  $"))
+       intQuantity = int(input("Please enter the quantity: "))
+       intInitialWeight = int(input("Please enter the weight per item(lbs): "))
 
-       strDescription = strDescription + " " + strPurchase
+       strDescription += strPurchase + ", "
 
-       intSubtotal = intSubtotal + (intPrice * intQuantity)
+       intTotalWeight += intInitialWeight
 
-       intShipping = (.25 * (intWeight * intQuantity))+ 5
+       intSubtotal += (intPrice * intQuantity)
 
-       intTax = .085 * (intSubtotal + intShipping)
+       intShipping += (.25 * (intInitialWeight * intQuantity))
 
-       intTotal = intSubtotal + intShipping + intTax
+       intTax += .085 * (intPrice * intQuantity)
+
+    intShipping += 5 # HC
+    intTotal = intSubtotal + intShipping + intTax
 
 
     print("You have purchased:", strDescription)
     print("The subtotal is $", intSubtotal)
     print("The Shipping and Handling costs are $", intShipping)
-    print("The Tax is $", intTax)
-    print("The Total is $", intTotal)
+    print("The Tax is ${0:0.2f}".format(intTax))
+    print("The Total is ${0:0.2f}".format(intTotal))
 
 
 main()
