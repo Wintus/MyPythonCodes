7cc1ce43e1abef11df4c8e6852e7aa6d02e2174b
Wintus <132e84e5@opayq.com>
2015-01-27 22:32:33 -0800

    Refactor
    in class

--- MyManalangHomework02Sec01.py
+++ MyManalangHomework02Sec01.py
@@ -1,33 +1,43 @@
 # Project:      Homework 02 (MaemichiManalangHomework02Sec01.py)
 # Name:         Maemichi, Yuya & Martina Manalang
-# Date:         04/28/14
-# Description:  This program will allow the user enter information about 3 items and receive their subtotal, tax, shipping and handling and total.
+# Date:         05/01/14
+# Description:  This program will allow the user enter information of 3 items
+#   and display their subtotal, tax, shipping and handling cost, and total.
 
 def main():
+
+    #Initialize accumulators
     strDescription = []
     fltSubtotal = 0.0
     fltShipping = 0.0
-    
-    for i in range(3):
-       strDescription += [str(input("Please enter the item description: "))]
-       fltPrice = float(input("PLease enter the price: $"))
-       intQuantity = int(input("Please enter the quantity: "))
-       fltWeight = float(input("Please enter the weight per item(lbs): "))
 
-       fltSubtotal += fltPrice * intQuantity
-       fltShipping += .25 * fltWeight * intQuantity
+    #Error checking
+    try:
+        for i in range(3):
+            #Get user inputs
+           strDescription += [str(input("Please enter the item description: "))]
+           fltPrice = float(input("PLease enter the price: $"))
+           intQuantity = int(input("Please enter the quantity: "))
+           fltWeight = float(input("Please enter the weight per item(lbs): "))
 
+           #Accumulating
+           fltSubtotal += fltPrice * intQuantity
+           fltShipping += .25 * fltWeight * intQuantity
+    except ValueError:
+        print("ERROR: Wrong Inputs captured")
+
+    #Process the rest of values
     fltShipping += 5 #Handling charge
     fltTax = .085 * fltSubtotal
     fltTotal = fltSubtotal + fltTax + fltShipping
 
+    #Display the result
     print()
     print("You have purchased: {}, {} and {}.".format(*strDescription))
     print("The subtotal is ${}.".format(fltSubtotal))
     print("The Shipping and Handling costs are ${}.".format(fltShipping))
     print("The Tax is ${:.2f}.".format(fltTax))
     print("The Total is ${:.2f}.".format(fltTotal))
-
 
 main()
 
