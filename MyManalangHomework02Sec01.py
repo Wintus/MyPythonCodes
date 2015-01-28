66d73f9538723e8246c58de596e5579014771716
Wintus <132e84e5@opayq.com>
2015-01-27 22:06:45 -0800

    Refactor
    in class

--- MyManalangHomework02Sec01.py
+++ MyManalangHomework02Sec01.py
@@ -4,37 +4,33 @@
 # Description:  This program will allow the user enter information about 3 items and receive their subtotal, tax, shipping and handling and total.
 
 def main():
-    strDescription = ""
+    strDescription = []
     intSubtotal = 0
-    intTotalWeight = 0
+##    intTotalWeight = 0
     intShipping = 0
     intTax = 0
     
     for i in range(3):
        strPurchase = str(input("Please enter the item description: "))
-       intPrice = int(input("PLease enter the price:  $"))
+       intPrice = int(input("PLease enter the price: $"))
        intQuantity = int(input("Please enter the quantity: "))
        intInitialWeight = int(input("Please enter the weight per item(lbs): "))
 
-       strDescription += strPurchase + ", "
-
-       intTotalWeight += intInitialWeight
-
+       strDescription += [strPurchase]
+##       intTotalWeight += intInitialWeight
        intSubtotal += (intPrice * intQuantity)
-
        intShipping += (.25 * (intInitialWeight * intQuantity))
-
        intTax += .085 * (intPrice * intQuantity)
 
-    intShipping += 5 # HC
+    intShipping += 5 # Handling Cost
     intTotal = intSubtotal + intShipping + intTax
 
-
-    print("You have purchased:", strDescription)
-    print("The subtotal is $", intSubtotal)
-    print("The Shipping and Handling costs are $", intShipping)
-    print("The Tax is ${0:0.2f}".format(intTax))
-    print("The Total is ${0:0.2f}".format(intTotal))
+    print()
+    print("You have purchased: {0}, {1} and {2}".format(*strDescription))
+    print("The subtotal is ${}.".format(intSubtotal))
+    print("The Shipping and Handling costs are ${}.".format(intShipping))
+    print("The Tax is ${0:0.2f}.".format(intTax))
+    print("The Total is ${0:0.2f}.".format(intTotal))
 
 
 main()
