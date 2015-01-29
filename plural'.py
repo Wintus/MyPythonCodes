b476fe862a983fe03c4555315cb0408b616b321f
Wintus <132e84e5@opayq.com>
2015-01-29 15:42:17 -0800

    Change generator to iterator of rules
    Dive into Python 3

--- plural'.py
+++ plural'.py
@@ -1,7 +1,7 @@
-'''Pluralize English nouns (stage 5)
+'''Pluralize English nouns (stage 6)
 
 Command line usage:
-$ python plural5.py noun
+$ python plural6.py noun
 nouns
 '''
 
@@ -14,17 +14,42 @@ def build_match_and_apply_functions(pattern, search, replace):
         return re.sub(search, replace, word)
     return [matches_rule, apply_rule]
 
-def rules(rules_filename):
-    with open(rules_filename, encoding='utf-8') as pattern_file:
-        for line in pattern_file:
-            pattern, search, replace = line.split(None, 3)
-            yield build_match_and_apply_functions(pattern, search, replace)
+class LazyRules:
+    rules_filename = 'plural6-rules.txt'
 
-def plural(noun, rules_filename='plural5-rules.txt'):
-    for matches_rule, apply_rule in rules(rules_filename):
+    def __init__(self):
+        self.pattern_file = open(self.rules_filename, encoding='utf-8')
+        self.cache = []
+
+    def __iter__(self):
+        self.cache_index = 0
+        return self
+
+    def __next__(self):
+        self.cache_index += 1
+        if len(self.cache) >= self.cache_index:
+            return self.cache[self.cache_index - 1]
+
+        if self.pattern_file.closed:
+            raise StopIteration
+
+        line = self.pattern_file.readline()
+        if not line:
+            self.pattern_file.close()
+            raise StopIteration
+
+        pattern, search, replace = line.split(None, 3)
+        funcs = build_match_and_apply_functions(
+            pattern, search, replace)
+        self.cache.append(funcs)
+        return funcs
+
+rules = LazyRules()
+        
+def plural(noun):
+    for matches_rule, apply_rule in rules:
         if matches_rule(noun):
             return apply_rule(noun)
-    raise ValueError('no matching rule for {0}'.format(noun))
 
 if __name__ == '__main__':
     import sys
