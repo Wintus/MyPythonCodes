6b38188f1e1daa9b9f8871493d78c694cc5dfb3d
Wintus <132e84e5@opayq.com>
2015-01-29 23:57:13 -0800

    Add test case to extend number range
    add known values

--- romantest.py
+++ romantest.py
@@ -1,11 +1,11 @@
-'''Unit test for roman7.py
+'''Unit test for roman9.py
 
 This program is part of 'Dive Into Python 3', a free Python book for
 experienced programmers.  Visit http://diveintopython3.org/ for the
 latest version.
 '''
 
-import roman7
+import roman9
 import unittest
 
 class KnownValues(unittest.TestCase):
@@ -64,66 +64,71 @@ class KnownValues(unittest.TestCase):
                      (3844, 'MMMDCCCXLIV'),
                      (3888, 'MMMDCCCLXXXVIII'),
                      (3940, 'MMMCMXL'),
-                     (3999, 'MMMCMXCIX'))
+                     (3999, 'MMMCMXCIX'),
+                     (4000, 'MMMM'),
+                     (4500, 'MMMMD'),
+                     (4888, 'MMMMDCCCLXXXVIII'),
+                     (4999, 'MMMMCMXCIX') )
 
     def test_to_roman_known_values(self):
         '''to_roman should give known result with known input'''
         for integer, numeral in self.known_values:
-            result = roman7.to_roman(integer)
+            result = roman9.to_roman(integer)
             self.assertEqual(numeral, result)
 
     def test_from_roman_known_values(self):
         '''from_roman should give known result with known input'''
         for integer, numeral in self.known_values:
-            result = roman7.from_roman(numeral)
+            result = roman9.from_roman(numeral)
             self.assertEqual(integer, result)
 
 class ToRomanBadInput(unittest.TestCase):
     def test_too_large(self):
         '''to_roman should fail with large input'''
-        self.assertRaises(roman7.OutOfRangeError, roman7.to_roman, 4000)
+        self.assertRaises(roman9.OutOfRangeError, roman9.to_roman, 5000)
 
     def test_zero(self):
         '''to_roman should fail with 0 input'''
-        self.assertRaises(roman7.OutOfRangeError, roman7.to_roman, 0)
+        self.assertRaises(roman9.OutOfRangeError, roman9.to_roman, 0)
 
     def test_negative(self):
         '''to_roman should fail with negative input'''
-        self.assertRaises(roman7.OutOfRangeError, roman7.to_roman, -1)
+        self.assertRaises(roman9.OutOfRangeError, roman9.to_roman, -1)
 
     def test_non_integer(self):
         '''to_roman should fail with non-integer input'''
-        self.assertRaises(roman7.NotIntegerError, roman7.to_roman, 0.5)
+        self.assertRaises(roman9.NotIntegerError, roman9.to_roman, 0.5)
 
-class RoundtripCheck(unittest.TestCase):
-    def test_roundtrip(self):
-        '''form_roman(to_roman(n))==n for all n'''
-        for integer in range(1, 4000):
-            numeral = roman7.to_roman(integer)
-            result = roman7.from_roman(numeral)
-            self.assertEqual(integer, result)
-            
 class FromRomanBadInput(unittest.TestCase):
     def test_too_many_repeated_numerals(self):
         '''from_roman should fail with too many repeated numerals'''
-        for s in ('MMMM', 'DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII'):
-            self.assertRaises(roman7.InvalidRomanNumeralError, roman7.from_roman, s)
+        for s in ('MMMMM', 'DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII'):
+            self.assertRaises(roman9.InvalidRomanNumeralError, roman9.from_roman, s)
 
     def test_repeated_pairs(self):
         '''form_roman should fail with repeated pairs of numerals'''
         for s in ('CMCM', 'CDCD', 'XCXC', 'XLXL', 'IXIX', 'IVIV'):
-            self.assertRaises(roman7.InvalidRomanNumeralError, roman7.from_roman, s)
+            self.assertRaises(roman9.InvalidRomanNumeralError, roman9.from_roman, s)
 
     def test_malformed_antecedents(self):
         '''from_roman should fail with malformed antecedents'''
         for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV',\
                   'MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
-            self.assertRaises(roman7.InvalidRomanNumeralError, roman7.from_roman, s)
+            self.assertRaises(roman9.InvalidRomanNumeralError, roman9.from_roman, s)
 
     def testBlank(self):
        '''from_roman should fail with blank string'''
-       self.assertRaises(roman7.InvalidRomanNumeralError, roman7.from_roman, '')
+       self.assertRaises(roman9.InvalidRomanNumeralError, roman9.from_roman, '')
             
+class RoundtripCheck(unittest.TestCase):
+    def test_roundtrip(self):
+        '''form_roman(to_roman(n))==n for all n'''
+        for integer in range(1, 5000):
+            numeral = roman9.to_roman(integer)
+            result = roman9.from_roman(numeral)
+            self.assertEqual(integer, result)
+            
+
 if __name__ == '__main__':
     unittest.main()
 
