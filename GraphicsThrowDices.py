ce168c6d6f6510db100fed203fdfbef129b16b66
Wintus <132e84e5@opayq.com>
2015-01-30 21:58:05 -0800

    Extend to throw 3 dices
    each faces apper randomly

--- GraphicsThrowDices.py
+++ GraphicsThrowDices.py
@@ -4,6 +4,7 @@
 # Description:  This program will displays the 5 side of a dice.
 
 from graphics import *
+import random
 
 def dice(intNumber, centerPoint, intSize, graphwin):
     '''create a dice with <intNumber> dots at <centerPoint> of <intSize> on <graphwin>'''
@@ -21,7 +22,7 @@ def dice(intNumber, centerPoint, intSize, graphwin):
     
     # initialize dots
     dot0, dot1, dot2, dot3, dot4, dot5 = tuple(range(6))
-    dots    = (dot0, dot1, dot2, dot3, dot4, dot5)
+    dots = (dot0, dot1, dot2, dot3, dot4, dot5)
     
     # prepare the coordinates for 1 dot to 6 dots in each lines
     points  = [\
@@ -41,7 +42,8 @@ def dice(intNumber, centerPoint, intSize, graphwin):
     
     # draw each dots by dot()
     for d, p in zip(dots, points[intNumber - 1]):
-        # zip(): [1, 2, 3, 4] & [a, b, c] -> [(1, a), (2, b), (3, c)] (omit the excess)
+        # zip(): [1, 2, 3, 4] & [a, b, c] -> [(1, a), (2, b), (3, c)]
+        # (omit the excess)
         d = dot(p, intR, graphwin)
         
     return box, dots # return a tuple
@@ -79,7 +81,8 @@ def inArea(rectangle, point):
     rightEnd    = rectangle.getP2().getX()
     topEnd      = rectangle.getP1().getY()
     bottomEnd   = rectangle.getP2().getY()
-    return leftEnd < pointX < rightEnd and topEnd < pointY < bottomEnd # return Boolean
+    return leftEnd < pointX < rightEnd \
+           and topEnd < pointY < bottomEnd # return Boolean
 
 def main():
     '''the main function'''
@@ -93,16 +96,18 @@ def main():
     textboxExit = textbox("EXIT", Point(intWidth * .9, intHeight * .9), \
                           80, 50, win) # (text, box)
     # draw Show button
-    textboxClick = textbox("Click to\n show dice", \
+    textboxClick = textbox("Click to\n throw dices", \
                            Point(intWidth // 2, intHeight // 2 + intSize), \
                            100, 50, win) # (text, box)
 
-    # entry of the number of the dice
-    entry = Entry(Point(intWidth // 2, intHeight // 2 - intSize * .9), 3)
-    entry.draw(win)
-
-    # and its tip
-    strText = "Enter the number of dots"
+##    # entry of the number of the dice
+##    entry = Entry(Point(intWidth // 2, intHeight // 2 - intSize * .9), 3)
+##    entry.setText('5')
+##    entry.draw(win)
+##
+##    # and its tip
+##    strText = "Enter the number of dots"
+    strText = "Throw 3 dices"
     textboxNumber = textbox(strText, \
                             Point(intWidth // 2, intHeight // 2 - intSize * 1.2), \
                             200, 30, win) # (text, box)
@@ -110,14 +115,21 @@ def main():
     # infinity loop to get mouse click unless push exit button
     while True:
         clicked = win.getMouse()
-        textboxNumber[0].setText(strText) # textboxNumber = (text, box)
+##        textboxNumber[0].setText(strText) # textboxNumber = (text, box)
         if inArea(textboxClick[1], clicked):
             # draw a dice
-            intNumber = int(entry.getText())
-            if 1 <= intNumber <= 6:
-                dice1 = dice(intNumber, Point(intWidth // 2, intHeight // 2), intSize, win)
-            else:
-                textboxNumber[0].setText("Invaild Input")
+##            intNumber = int(entry.getText())
+##            if 1 <= intNumber <= 6:
+                dice0 = dice(random.randint(1, 6), \
+                             Point(intWidth // 4 * .8, intHeight // 2), \
+                             intSize, win)
+                dice1 = dice(random.randint(1, 6), \
+                             Point(intWidth // 2, intHeight // 2), intSize, win)
+                dice2 = dice(random.randint(1, 6), \
+                             Point(intWidth *3.2 // 4, intHeight // 2), \
+                             intSize, win)
+##            else:
+##                textboxNumber[0].setText("Invaild Input")
         elif inArea(textboxExit[1], clicked):
             break # jump out from the loop
      
