c1626c8b4497f0f1be2bc1e49da7748b6ac84a91
Wintus <132e84e5@opayq.com>
2015-01-30 20:31:07 -0800

    Erase trace
    refactor sample a bit as well

--- TailRecursive.py
+++ TailRecursive.py
@@ -5,43 +5,27 @@ from functools import wraps
 class Continue(object):pass
 
 def tail_recursive(func):
-    print('Initialize')
     first_call = True
     CONTINUE = Continue()
     args_kwd = None
     
     @wraps(func)
     def _tail_recursive(*args, **kwd):
-        print('inner_func')
-        print('given_args:\t', args, kwd)
         nonlocal func, first_call, CONTINUE, args_kwd # closure
-        if first_call == True:
-            print('first_call')
+        if first_call:
             first_call = False
             try:
-                count = 0
                 while True:
-                    count += 1
-                    print('loop:', count)
-                    print('call_func')
                     result = func(*args, **kwd)
-                    # label: A
                     if result is CONTINUE:  # update arguments
-                        print('continued')
                         args, kwd = args_kwd
-                        print('updated_args:\t', args, kwd)
                     else: # last call
-                        print('end')
                         return result
             finally:
-                print('finally')
                 first_call = True # prepare for the next call
         else: # return the arguments of the tail call
-            print('non-first_call')
             args_kwd = args, kwd
-            print('tail-call_args:\t', args_kwd)
-            print('continue')
-            return CONTINUE # goto A
+            return CONTINUE
     return _tail_recursive
 
 
@@ -50,7 +34,7 @@ if __name__ == '__main__':
     def sum_to(n, acc=0):
         return acc if n == 0 else sum_to(n-1, acc+n)
 
-    n = 10#0000
+    n = 100000
     print("The sum from 1 to {}:".format(n), sum_to(n))
     print()
     
@@ -60,13 +44,12 @@ if __name__ == '__main__':
             return _fib(b, a+b, n-1) if n > 0 else a
         return _fib(0, 1, n)
 
-    n = 10#00
-    print("fibbnacci number of the {}th:".format(n), fib(n))
+    print("fibbnacci number of 1000th:", fib(1000))
     print()
 
     @tail_recursive
     def fact(n, acc=1):
         return acc if n == 0 else fact(n-1, n*acc)
-    
-    n = 10#0
-    print("factorial of {}:".format(n), fact(n))
+
+    print("factorial of 100:", fact(100))
+    print()
