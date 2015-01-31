a423fb67a1e4e2478b395a117e8a7883aa534c8c
Wintus <132e84e5@opayq.com>
2015-01-30 22:27:21 -0800

    Change upper bound from 2^n to n

--- primes.py
+++ primes.py
@@ -1,19 +1,20 @@
 #Calc primes
 
-#calc the list of primes up to 2^n
+#calc the list of primes
 def prime(n):
     ps=[2,3]
-    for i in range(3,2**n):
+    for i in range(3,n):
         if i%6==1 or i%6==5:
             for p in ps:
                 if i%p==0:
                     break
-                ps.append(i)
+                else:
+                    ps.append(i)
             
-    print("There are {1} pirmes up to {2}.\nThe list of primes: {0}".format(ps, len(ps), 2**n))
+    print("There are {1} pirmes up to {2}.\nThe list of primes: {0}".format(ps, len(ps), n))
 
 def main():
-    n = eval(input("Primes up to 2 ^ "))
+    n = eval(input("Primes up to "))
     prime(n)
 
 main()
