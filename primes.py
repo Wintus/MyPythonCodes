cd77141832fdb3ee91aca726b00081a527243046
Wintus <132e84e5@opayq.com>
2015-01-30 22:24:39 -0800

    Refactor output
    erase main()

--- primes.py
+++ primes.py
@@ -2,18 +2,17 @@
 
 #calc the list of primes up to 2^n
 def prime(n):
-    ps=[2,3]
+    ps=[2]
     for i in range(3,2**n):
         if i%6==1 or i%6==5:
             for p in ps:
                 if i%p==0:
                     break
+            else:
                 ps.append(i)
-            
-    print("There are {1} pirmes up to {2}.\nThe list of primes: {0}".format(ps, len(ps), 2**n))
+    return ps
 
-def main():
-    n = eval(input("Primes up to 2 ^ "))
-    prime(n)
-
-main()
+n   = int(input("Primes up to 2 ^ "))
+ps  = prime(n)
+print("There are {1} pirmes up to {2}.\nThe list of primes: {0}" \
+    .format(ps, len(ps), 2**n))
