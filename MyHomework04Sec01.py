ade5b81cfa28d4f6da5367d5100ad460c1efa03c
Wintus <132e84e5@opayq.com>
2015-01-30 19:53:20 -0800

    Refactor and Impletent output
    change options of functions
    step back to use switch
    change variable name to follow convension in class

--- MyHomework04Sec01.py
+++ MyHomework04Sec01.py
@@ -3,97 +3,139 @@
 # Date:         05/14/14
 # Description:  This program determines the cost of sending out coffee.
 
-def coffee_price(order, lbs=0.0):
+def coffee_price(strOrder, fltPounds):
     '''calc the coffee price'''
     fltJB = 10.50 # $/lbs - Jonestown Brew
     fltPJ = 16.95 # $/lbs - Plymouth Jolt
-    coffee = {"Jonestown Brew":10.05, "Plymouth Jolt":16.95,}
-    return coffee[order] * lbs
+    if strOrder == 'J':
+        return fltJB * fltPounds
+    elif strOrder == 'P':
+        return fltPJ * fltPounds
+    else:
+        raise ValueError
 
-def tax(amount, region="the Others"):
+def tax(fltAmount, strState):
     '''calc the tax'''
-    taxes = {"Washington":9, "California":9, "Texas":9, \
-             "Oregon":0, "Florida":0, 'the Others':7,}
-    return amount * (1 + taxes[region] / 100)
+    if strState == 'WA' or 'CA' or 'TX':
+        return fltAmount * .09
+    elif strState == 'OR' or 'FL':
+        return fltAmount * .00
+    elif len(strState) == 2:
+        return fltAmount * .07
+    else:
+        raise ValueError
 
-def shipping_and_handling_cost(lbs, option="Standard"):
+def shipping_and_handling_cost(fltPounds, strOption):
     '''calc the shipping and handling cost'''
     fltShippingRate = 0.93 # $/lbs
-    fltHandlingCost = 2.50 # $/lbs
-    shipping_methods = {"Overnight":20.00, "2-Day":13.00, "Standard":0.00,}
-    return fltShippingRate * lbs + fltHandlingCost + \
-           shipping_methods[option]
+    fltHandlingCost = 2.50 # $/order
+    if strOption == 'O':
+        fltShipping = 20.00
+    elif strOption == '2':
+        fltShipping = 13.00
+    elif strOption == 'S':
+        fltShipping = 0.00
+    else:
+        raise ValueError
+    fltShipping += fltShippingRate * fltPounds + fltHandlingCost
+    return fltShipping
 
-def payment(subtotal, option="cash"):
+def payment(fltSubtotal, intOption):
     '''calc the amount at the payment'''
-    options = {"Paypal":3, "Credit Cards":5, "Check":-2, "cash":0,}
-    return subtotal * (1 + options[option] / 100)
-
-##def counter(total, func, arg):
-##    total += round(func, total, arg)
+    # 0:Cash, 1:PayPal, 2:Credit Cards, 3:Check
+    if intOption == 1:
+        intRate = 3 # PayPal
+    elif intOption == 2:
+        intRate = 5
+    elif intOption == 3:
+        intRate = -2
+    else:
+        intRate = 0
+    return fltSubtotal * (1 + intRate / 100)
 
 def main():
     '''the main function'''
     print("Coffee shop")
-##    finished = False
-    total = 0.0
-
-    def interface(func, dic, key, prompt):
-        '''Abstraction of repeated patterns'''
-        nonlocal total
-        print(prompt)
-        arg = dic[key]
-        print('>', arg)
-        total += func(total, arg)
-        print()
-        
-##    while not finished:
+       
     while True:
         try:
-            total = 0.0
+            fltTotal = 0.0
 
-            coffees = {'J':"Jonestown Brew", 'P':"Plymouth Jolt",}            
+            # get inputs and calculate
+            # coffee
             print("Jonestown Brew: J, Plymouth Jolt: P")
-            coffee = str(input("Coffee?: ")).upper()
-##            if not coffee:
-##                raise ValueError("Empty input")
-##            elif coffee == 'J':
-##                order = "Jonestown Brew"
-##            elif coffee == 'P':
-##                order = "Plymouth Jolt"
-##            else:
-##                raise ValueError("Invalid input")
-            print('>', coffees[coffee])
+            strCoffee = str(input("Coffee?: ")).upper()
+            if not strCoffee:
+                raise ValueError("Empty input")
+            elif strCoffee == 'J':
+                strCoffeeName = "Jonestown Brew"
+            elif strCoffee == 'P':
+                strCoffeeName = "Plymouth Jolt"
+            else:
+                raise ValueError("Invalid input")
+            print('>', strCoffeeName)
             print()
-            lbs = float(input("Amount? (lbs): "))
-            print('>', lbs)
-            total += coffee_price(order, lbs)
-            print()
-
-            city = str(input("City?: ")).title()
-            region = str(input("State?: ")).upper()
-            states = {'W':"Washington", 'C':"California", 'T':"Texas", \
-                      'O':"Oregon", 'F':"Florida", '':"the Others",}
-            print('>', states[region])
-            total += tax(total, states[region])
+            fltPounds = float(input("Amount? (lbs): "))
+            print('>', fltPounds)
+            fltCoffeePrice = coffee_price(strCoffee, fltPounds)
             print()
 
-            option = int(input("Payment method?: "))
-            options = ("cash", "Paypal", "Credit Cards", "Check",)
-            print('>', options[option])
-            total += payment(total, options[option])
+            # city and state
+            print("Enter the City and State for shipping")
+            strCity = str(input("City?: ")).title()
+            strState = str(input("State?: ")).upper()
+            print('>', strCity)
+            print('>', strState)
             print()
 
-            method = str(input("Shipping option?: ")).upper()
-            methods = {'O':"Overnight", '2':"2-Day", 'S':"Standard",}
-            print('>', methods[method])
-            total += shipping_and_handling_cost(lbs, methods[method])
+            # shiping method
+            print("Overnight: O, 2-Days: 2, Standard:, S")
+            strOption = str(input("Shipping method?: ")).upper()
+            print('>', strOption)
+            fltShipping = shipping_and_handling_cost(fltPounds, strOption)
+            fltSubtotal = fltCoffeePrice + fltShipping
+            # tax
+            fltTax = tax(fltSubtotal, strState)
             print()
 
-            total = round(total, 2)
-            print("Total:", total)
+            # payment option
+            print("0:Cash, 1:PayPal, 2:Credit Cards, 3:Check")
+            intOption = int(input("Payment option?: "))
+            options = ("cash", "PayPal", "Credit Cards", "Check",)
+            print('>', options[intOption])
             print()
-##            finished = True
+
+            fltTotal = fltSubtotal + payment(fltTotal, intOption) + fltTax
+            fltTotal = round(fltTotal, 2)
+
+            # display the order
+            print("YOUR ORDER")
+            print("Coffee:", strCoffeeName)
+            print("Amount: {:.2f}lbs.".format(fltPounds))
+            print("Coffee price: {:.2f}".format(fltCoffeePrice))
+            print("Shipping & Handling costs: {:.2f}".format(fltShipping))
+            print("Subtotal: {:.2f}".format(fltSubtotal))
+            print("City:", strCity)
+            print("State:", strState)
+            if strOption == 'O':
+                print("Shipping method: Overnight")
+            elif strOpiton == '2':
+                print("Shipping method: 2-Days")
+            elif strOption == 'S':
+                print("Shipping method: Standard")
+
+            if intOption == 1:
+                print("Payment option: PayPal")
+            elif intOption == 2:
+                print("Payment option: Credit Cards")
+            elif intOption == 3:
+                print("Payment option: Check")
+            else:
+                print("Payment option: Cash")
+            print("Tax: {:.2f}".format(fltTax))
+            print("Total:", fltTotal)
+            
+            print()
             break
         
         except ValueError:
